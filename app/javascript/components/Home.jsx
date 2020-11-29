bec881636f50b322222e492bf62132117714b43a
michael <michael.alexander.benson@gmail.com>
2020-11-29 16:24:43 -0500

    basic form

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import InsuranceForm from "../components/InsuranceForm.jsx"
+import girl from "images/girl-pillow-wide.png";
+function Home() {
+  const [count, setFormVersion] = useState("short");
+    return (
+    <div className="home-container">
+      <div className="hero-container">
+        <div className="hero-content">
+          <h2> Expert Help For Your Emotional Health </h2>
+            <InsuranceForm
+              girl =  {girl}
+            />
+        </div>
 
-export default () => (
-  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-    <div className="jumbotron jumbotron-fluid bg-transparent">
-      <div className="container secondary-color">
-        <h1 className="display-4">Food Recipes</h1>
-        <p className="lead">
-          A curated list of recipes for the best homemade meal and delicacies.
-        </p>
-        <hr className="my-4" />
-        <Link
-          to="/recipes"
-          className="btn btn-lg custom-button"
-          role="button"
-        >
-          View Recipes
-        </Link>
+        <div className="girl-wrap">
+          <img src={girl}></img>
+        </div>
       </div>
     </div>
-  </div>
-);
+    )
+}
+
+export default Home
+
+
---
+++ app/javascript/components/InsuranceForm.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+export default class InsuranceForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      name: "",
+      relationship: 'self',
+      group_name: "",
+      state: "",
+      group_id: "",
+      policy_id: "",
+    };
+
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit = (event) => {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <section>
+        <label>
+          Email:
+        </label>
+          <input
+            name="email"
+            type="email"
+            value={this.state.email}
+            onChange={this.handleInputChange} />
+        </section>
+        <section>
+          <label>
+            Password:
+          </label>
+            <input
+              name="password"
+              type="password"
+              value={this.state.email}
+              onChange={this.handleInputChange} />
+        </section>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
